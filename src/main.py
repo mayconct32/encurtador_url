8a41cdaffd7b8420f5045aa81975a4a062351b99
mayconct32 <maycon326754@gmail.com>
2026-06-20 15:20:10 -0300

    feat: Adds Pydantic Validations

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.post("/shorten-url/")
-def shorten_url():
+class InputURL(BaseModel):
+    long_url: str
+
+
+class ReplyURL(BaseModel):
+    short_url: str
+
+
+@app.post("/shorten-url/", response_model=ReplyURL)
+def shorten_url(long_url: InputURL):
     pass
+
 
 @app.get("/{path}")
 def access_short_url(path: str):
